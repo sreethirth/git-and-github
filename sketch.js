606a18f8a27c554ec0ab03bbd391a2ff98eb3796
sreethirth <sreethirth9889@gmail.com>
2020-11-09 19:44:36 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+   platform=new Ground(180,305,300,170)
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
